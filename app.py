4c41e8f1aab129d820a4f38313a5afeb1b975d45
Larry Hope <larry@sno.ai>
2023-10-17 08:56:53 -0700

    Init test

---
+++ app.py
@@ -0,0 +1,10 @@
+from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
+
+import openai
+
+openai.api_key = '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'
+
+config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
+user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding"})
+user_proxy.initiate_chat(assistant, message="Plot a chart of TESLA stock price change YTD")
